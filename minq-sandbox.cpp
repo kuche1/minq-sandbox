a74d65d64a2b979889a9003c40bea31cde882073
kuche1 <znamsiparolata4esno@abv.bg>
2024-06-03 10:18:06 +0300

    update

--- minq-sandbox.cpp
+++ minq-sandbox.cpp
@@ -4,10 +4,12 @@
 // make it so that instead of invalidating the syscall id we skip the execution and overwrite the return code
 //
 // make it so that syscall settings are presistent over sandbox restart
+//
+// make it so that we can pass a flag that enables networking
 
 // TOD0
 //
-// make it so that `parse_cmdline`, ... are actual functions, so that we know the variables used
+// NONE
 
 #include "inc/1_includes_namespaces.cpp"
 
